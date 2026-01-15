3f8d2bd5364c3ff1b3afe3c499bcadfb40627723
Sandro Schnetzer <84786681+Crim5on@users.noreply.github.com>
2026-01-15 15:53:21 +0100

    Refactor BubbleSort function and update sorting logic
    Refactored BubbleSort function to return a boolean value and updated its parameter type. Adjusted sorting logic to correctly iterate through the array.

--- BubbleSort.pas
+++ BubbleSort.pas
@@ -1,7 +1,7 @@
 (* Function Declaration *)
-FUNCTION BubbleSort : INT
-VAR_INPUT
-	numbers : REFERENCE TO ARRAY [0..9] OF INT;
+FUNCTION BubbleSort : BOOL
+VAR_IN_OUT
+	numbers : ARRAY[*] OF INT;
 END_VAR
 VAR
 	hold : INT;
@@ -13,8 +13,8 @@ END_VAR
 (* Function Implementation *)
 len := TO_INT(ABS(UPPER_BOUND(numbers, 1) - LOWER_BOUND(numbers, 1)) + 1);	
 
-FOR i := 0 TO len-1 BY 1 DO
-	FOR j := i TO len BY 1 DO
+FOR i := 0 TO len-2 BY 1 DO
+	FOR j := 0 TO len-2-i BY 1 DO
 		IF numbers[j] > numbers[j+1] THEN
 			hold := numbers[j];
 			numbers[j] := numbers[j+1];
@@ -22,3 +22,24 @@ FOR i := 0 TO len-1 BY 1 DO
 		END_IF
 	END_FOR;  
 END_FOR;
+
+BubbleSort := TRUE;
+
+
+
+(* --------------------------------------------------------------------------------------------- *)
+
+
+
+(* Main Declaration *)
+PROGRAM PLC_PRG
+VAR
+	isSorted : BOOL := FALSE;
+	myNumbers: ARRAY[0..9] OF INT := [174, 205, 239, 83, 11, 249, 21, 103, 42, 115];
+END_VAR
+
+
+(* Function Call *)
+IF NOT isSorted THEN
+	isSorted := BubbleSort(numbers := myNumbers);
+END_IF
